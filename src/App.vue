c8f0a74a128eb0f6f7d0f93c5399964ea5140ec8
mark <markpackham1@gmail.com>
2022-09-16 09:22:40 +0100

    Setup for Vue3 Weather App learned from https://www.youtube.com/watch?v=TsQVdEekvtE&list=PL4cUxeGkcC9hfoy8vFQ5tbXO3vY0xhhUZ&index=2

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <RouterView />
+  </div>
+</template>
+
+<script setup>
+import { RouterView } from "vue-router";
+</script>
+
+
+<style>
+.page-enter-active {
+  transition: 600ms ease all;
+}
+.page-enter-from {
+  opacity: 0;
+}
+</style>
