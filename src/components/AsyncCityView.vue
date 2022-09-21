63f9faddfcab942e8698ce6447fdcd1ec00283c7
mark <markpackham1@gmail.com>
2022-09-21 15:51:47 +0100

    get weatherData
    https://youtu.be/M66Vo8P7WSw?t=316

--- src/components/AsyncCityView.vue
+++ src/components/AsyncCityView.vue
@@ -7,4 +7,17 @@
 <script setup>
 import axios from "axios";
 import { useRoute } from "vue-router";
+
+const route = useRoute();
+const getWeatherData = async () => {
+    try{
+        const weatherData = await axios.get(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${route.query.lat}&lon=${route.query.lng}&exclude={part}&appid=7efa332cf48aeb9d2d391a51027f1a71&units=metric`
+    );
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+
 </script>
