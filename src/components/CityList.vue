8bb95e13668e4b1a8c65554bf73f2519365d2555
mark <markpackham1@gmail.com>
2022-09-23 19:28:23 +0100

    get cities from City List
    https://youtu.be/Dr0r9TitMS8?t=379

--- src/components/CityList.vue
+++ src/components/CityList.vue
@@ -14,20 +14,28 @@ import axios from "axios";
 import { ref } from "vue";
 import { useRouter } from "vue-router";
 import CityCard from "./CityCard.vue";
+
 const savedCities = ref([]);
-
-const getCities = () => {
-    if (localStorage.getItem('savedCities')) {
-        savedCities.value = JSON.parse(localStorage.getItem('savedCities'))
-    }
-
-    const requests = [];
-    savedCities.value.forEach((city) => {
-        requests.push(
-            axios.get(
-                `https://api.openweathermap.org/data/2.5/weather?lat=${city.coords.lat}&lon=${city.coords.lng}&appid=7efa332cf48aeb9d2d391a51027f1a71&units=imperial`
-            )
+const getCities = async () => {
+    if (localStorage.getItem("savedCities")) {
+        savedCities.value = JSON.parse(
+            localStorage.getItem("savedCities")
         );
-    });
-}
+        const requests = [];
+        savedCities.value.forEach((city) => {
+            requests.push(
+                axios.get(
+                    `https://api.openweathermap.org/data/2.5/weather?lat=${city.coords.lat}&lon=${city.coords.lng}&appid=7efa332cf48aeb9d2d391a51027f1a71&units=imperial`
+                )
+            );
+        });
+        const weatherData = await Promise.all(requests);
+        weatherData.forEach((value, index) => {
+            savedCities.value[index].weather = value.data;
+        });
+    }
+};
+
+await getCities();
+
 </script>
