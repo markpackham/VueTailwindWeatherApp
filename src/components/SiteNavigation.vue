f42643af2b54616d352f83cb2b3821c786108a91
mark <markpackham1@gmail.com>
2022-09-23 18:03:23 +0100

    tidy up code layout

--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -1,16 +1,6 @@
 <template>
   <header class="sticky top-0 bg-weather-primary shadow-lg">
-    <nav
-      class="
-        container
-        flex flex-col
-        sm:flex-row
-        items-center
-        gap-4
-        text-white
-        py-6
-      "
-    >
+    <nav class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6">
       <RouterLink :to="{ name: 'home' }">
         <div class="flex items-center gap-3">
           <i class="fa-solid fa-sun text-2xl"></i>
@@ -19,30 +9,12 @@
       </RouterLink>
 
       <div class="flex gap-3 flex-1 justify-end">
-        <i
-          class="
-            fa-solid fa-circle-info
-            text-xl
-            hover:text-weather-secondary
-            duration-150
-            cursor-pointer
-          " @click="toggleModal"
-        ></i>
-        <i
-          class="
-            fa-solid fa-plus
-            text-xl
-            hover:text-weather-secondary
-            duration-150
-            cursor-pointer
-          "
-        ></i>
+        <i class="fa-solid fa-circle-info text-xl hover:text-weather-secondary duration-150 cursor-pointer"
+          @click="toggleModal"></i>
+        <i class="fa-solid fa-plus text-xl hover:text-weather-secondary duration-150 cursor-pointer"></i>
       </div>
 
-      <BaseModal
-        :modalActive="modalActive"
-        @close-modal="toggleModal"
-      >
+      <BaseModal :modalActive="modalActive" @close-modal="toggleModal">
         <div class="text-black">
           <h1 class="text-2xl mb-1">About:</h1>
           <p class="mb-4">
@@ -77,14 +49,14 @@
     </nav>
   </header>
 </template>
-  
-  <script setup>
+
+<script setup>
 import { RouterLink } from "vue-router";
 import { ref } from "vue";
 import BaseModal from "./BaseModal.vue";
 
 const modalActive = ref(null);
-const toggleModal = () =>{
+const toggleModal = () => {
   modalActive.value = !modalActive.value;
-}
+};
 </script>
