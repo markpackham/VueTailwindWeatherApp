31d58987f1d28a1dd92875706ad5947a4a04cef7
mark <markpackham1@gmail.com>
2022-09-26 14:07:05 +0100

    add City Card Skeleton component to replace "Loading" text
    https://youtu.be/8qqCLeArGgc?t=266

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -23,7 +23,7 @@
       <Suspense>
         <CityList />
         <template #fallback>
-          <p>Loading...</p>
+          <CityCardSkeleton />
         </template>
       </Suspense>
     </div>
@@ -35,6 +35,7 @@ import { ref } from "vue";
 import axios from "axios";
 import { useRouter } from "vue-router";
 import CityList from "../components/CityList.vue";
+import CityCardSkeleton from "../components/CityCardSkeleton.vue";
 
 const router = useRouter();
 const previewCity = (searchResult) => {
