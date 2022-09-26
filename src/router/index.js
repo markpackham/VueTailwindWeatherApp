5e849fb8fed9f11d71257fba2134e3b6d57ad6bf
mark <markpackham1@gmail.com>
2022-09-26 15:03:43 +0100

    show city and state in page title

--- src/router/index.js
+++ src/router/index.js
@@ -25,7 +25,10 @@ const router = createRouter({
 });
 
 router.beforeEach((to, from, next) => {
-  document.title = `${to.meta.title} | The Local Weather`;
+  document.title = `${
+    to.params.state ? `${to.params.city}, ${to.params.state}` : to.meta.title
+  } | The Local Weather`;
+  next();
 });
 
 export default router;
